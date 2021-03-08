82d8e83b5684a1e515ed621cbaf2e10112bae191
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-08 13:11:21 -0500

    setting up server

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const app = require express();
+const app = express();
 const PORT = process.env.PORT || 3002;
+const notes = require('./db/db.json')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
