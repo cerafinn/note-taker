d7e60e40abc337c05553bdc5f8b697068d6b3b2b
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-08 14:52:16 -0500

    setting up routes and functions for notes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3002;
-const notes = require('./db/db.json');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes')
 
@@ -12,14 +11,6 @@ app.use(express.static('public'));
 // Use routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-
-app.get('/api/notes', (req, res) => {
-  let results = notes
-  if (req.query) {
-    results = filterByTitle(req.query, results)
-  }
-  res.json(results);
-});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
