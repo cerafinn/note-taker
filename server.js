43e4370938911d09a79ac0e0c695b98c6f460557
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-08 14:27:12 -0500

    setting up server

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3002;
-const { notes } = require('./db/db.json')
+const notes = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+function filterByTitle(query, notesArray) {
+  let filteredResults = notesArray;
+  if(query.title) {
+    filteredResults = filteredResults.filter(note => note.title === query.title);
+  };
+  return filteredResults
+}
+
 app.get('/api/notes', (req, res) => {
-  let results = notes;
-  console.log(req.query);
+  let results = notes
+  if (req.query) {
+    results = filterByTitle(req.query, results)
+  }
   res.json(results);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
