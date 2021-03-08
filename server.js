dcfa97a2f3d8090f06b92bb70c9e4c763957a325
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-08 13:03:31 -0500

    setting up server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = require express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
