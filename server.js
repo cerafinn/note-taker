3a52c40a6f6a0f03619d477cda8c29b3289e3030
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-08 18:54:42 -0500

    moved routes to server.js due to issues in routes folder

--- server.js
+++ server.js
@@ -1,16 +1,58 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs')
 const app = express();
 const PORT = process.env.PORT || 3002;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes')
+const uuid = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Use routes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
+  return res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  let newNote = req.body;
+  newNote.id = uuid.v4();
+  notes.push(newNote);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json(notes);
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(notes[Number(req.params.id)]);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteID = req.params.id;
+
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json(notes);
+})
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
