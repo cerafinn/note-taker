72cdef4df7d88356858f571b11a42022a513cadc
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-13 13:30:09 -0500

    updates to routes

--- routes.js
+++ routes.js
@@ -3,6 +3,18 @@ const fs = require('fs');
 const path = require('path');
 const uuid = require('uuid');
 
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 router.get('/api/notes', (req, res) => {
   let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
   return res.json(notes);
@@ -28,17 +40,5 @@ router.delete('/api/notes/:id', (req, res) => {
   fs.writeFileSync('./db/db.json', JSON.stringify(newNotes));
   res.json(notes);
 });
-
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-// router.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, './public/index.html'));
-// });
 
 module.exports = router;
