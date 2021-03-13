94317ba6438f9d7f25e9895e91a5fdd1fcca92bb
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-13 13:28:56 -0500

    updates to routes

--- routes.js
+++ routes.js
@@ -37,8 +37,8 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// router.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/index.html'));
+// });
 
 module.exports = router;
