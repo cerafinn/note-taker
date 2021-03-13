658dcb2bcbb431cc8ef9b7656f51e4d64409215e
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-13 13:27:06 -0500

    updates to routes

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3002;
-const apiRoutes = require('./routes/routes');
-const htmlRoutes = require('./routes/htmlRoutes')
+const routes = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(apiRoutes);
-app.use(htmlRoutes);
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
