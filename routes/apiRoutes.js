4e6e91a98654972f00bc028bc5dafbde26a36266
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-01 10:19:04 -0400

    fixed issue with post and new note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,29 +2,29 @@ const router = require('express').Router();
 const fs = require('fs');
 const uuid = require('uuid');
 
-router.get('/api/notes', (req, res) => {
-  let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
+router.get('/api/notes', async (req, res) => {
+  let notes = await JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
   return res.json(notes);
 });
 
-router.post('/api/notes', (req, res) => {
-  let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+router.post('/api/notes', async (req, res) => {
+  let notes = await JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
   let newNote = req.body;
   newNote.id = uuid.v4();
   notes.push(newNote);
 
-  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  await fs.writeFileSync('./db/db.json', JSON.stringify(notes));
   res.json(notes);
 })
 
-router.delete('/api/notes/:id', (req, res) => {
-  let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+router.delete('/api/notes/:id', async (req, res) => {
+  let notes = await JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
   const newNotes = notes.filter( note => {
-    return note.id !==req.params.id;
+    return note.id !== req.params.id;
   })
 
-  fs.writeFileSync('./db/db.json', JSON.stringify(newNotes));
+  await fs.writeFileSync('./db/db.json', JSON.stringify(newNotes));
   res.json(notes);
 });
 
